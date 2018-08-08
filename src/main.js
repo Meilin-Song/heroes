3967dd1dda846564e186fa0735118c49ac550bad
Meilin-Song <meilin_song@163.com>
2018-08-08 20:19:52 +0800

    完成首页

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+//导入文件 定义变量  from
 import Vue from 'vue'
 import App from './App.vue'
+
+//导入样式  直接导入文件不需要定义变量
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
